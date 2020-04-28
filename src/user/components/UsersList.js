4841a5e81189029711679ff0c5b7d57ac55bcc07
RaidenDilan <raiden18@me.com>
2020-04-28 17:26:27 +0100

    Adding a Main Header

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
 import UserItem from './UserItem/UserItem';
+import Card from '../../shared/components/UIElements/Card';
 import './UsersList.css';
 
 const UsersList = props => {
-  if (props.items.length === 0) return (
-    <div className='center'>
-      <h2>No users found.</h2>
-    </div>
-  );
+  if (props.items.length === 0) {
+    return (
+      <div className='center'>
+        <Card>
+          <h2>No users found.</h2>
+        </Card>
+      </div>
+    );
+  }
 
   return (
     <ul className='users-list'>
